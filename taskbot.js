f00fa168d5e68837e948ee217a6e4ea06a9e4feb
louisnovick <louisnovick@hotmail.com>
2016-09-15 01:58:19 -0400

    setup initial convo to add tasks

--- taskbot.js
+++ taskbot.js
@@ -6,7 +6,6 @@
 
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
-
 if (!process.env.token) {
   console.log('Error: Specify token in environment');
   process.exit(1);
@@ -23,15 +22,50 @@ var bot = controller.spawn({
   token: process.env.token
 }).startRTM();
 
-
 // Add a task to the bots list
-controller.hears(['add task'], 'direct_message', function(bot, message) {
-  bot.reply(message,'Ok, what do you need to do today?');
+controller.hears(['add tasks'], 'direct_message', function(bot, message) {
+  bot.startConversation(message, function(err,convo) {
+    convo.ask('Type the word "done" once all tasks are added.', [
+      {
+        pattern: 'done',
+        callback: function(response, convo) {
+          // since no further messages are queued after this,
+          // the conversation will end naturally with status === ‘completed’
+          convo.next();
+        },
+      }, {
+        default: true,
+        callback: function(response, convo) {
+          convo.say('Task added, "' + response.text + '".');
+          convo.repeat();
+          convo.next();
+        },
+      },
+    ]);
+    convo.on('end', function(convo) {
+      if (convo.status == 'completed') {
+        var res = convo.extractResponses();
+        var value = convo.extractResponse('key');
+      }
+    });
+    /*bot.startConversation(message, function(err,convo) {
+      convo.ask('Type the word "done" once all tasks are added.', function(response, convo) {
+        if(response.text !== 'done') {
+          convo.say('Task added, "' + response.text + '".');
+          convo.repeat();
+        } else {
+          convo.say('All tasks added.');
+        }
+
+        convo.next();
+      })
+    });*/
+  });
 });
 
 // Show all tasks from the bots list
 controller.hears(['see tasks'], 'direct_message', function(bot, message) {
-  bot.reply(message,'Here are all your tasks for today.');
+  bot.reply(message,'Here are all your tasks for today.\n' + res + value );
 });
 
 // Clear all tasks from the bots list
